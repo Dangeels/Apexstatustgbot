874455b2906ac69979bca987877d482f4ea2c95e
Dangeels <geels.sasha@mail.ru>
2023-04-11 16:12:29 +0300

    Summary?

--- apex.py
+++ apex.py
@@ -7,6 +7,6 @@ def req(x, y=None):
     return requests.get(f'https://api.mozambiquehe.re/{x}?{y}&auth={config.ApexToken}').json() if y else requests.get(f'https://api.mozambiquehe.re/{x}?auth={config.ApexToken}').json()
 
 def alplayer(player, platform):
-    return requests.get(f'https://api.mozambiquehe.re/bridge?auth={config.BotToken}&player={player}&platform={platform}').json()
+    return requests.get(f'https://api.mozambiquehe.re/bridge?auth={config.ApexToken}&player={player}&platform={platform}').json()
 
 
